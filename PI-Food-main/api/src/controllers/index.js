c23710dde8fc70b09f9384de9e9376b3cea7a85a
Juan Esteban <juancho50301@gmail.com>
2023-04-25 18:45:02 -0500

    'primerControllerRoute'

--- PI-Food-main/api/src/controllers/index.js
+++ PI-Food-main/api/src/controllers/index.js
@@ -6,11 +6,30 @@ const{URL, API_KEY} = process.env;
 
 
 
-const getRecipeById = async () => {
+const getRecipeById = async (id) => {
 
-   const response = await axios.get(`${URL}apiKey=${API_KEY}&addRecipeInformation=true&number=10`);   
-    
-   console.log(response.data); 
+   const response = await axios.get(`${URL}${id}/information?apiKey=${API_KEY}`);   
+
+   const data = response.data;
+
+   //console.log(data);
+
+   if(!data) throw new Error(`Not Found`);
+
+   const recipe = {
+    id: data.id,
+    name: data.title,
+    summary: data.summary.replace(),
+    healthscore: data.healthScore,
+    steps: data.instructions,
+    vegetarian: data.vegetarian,
+    vegan: data.vegan,
+    glutenFree: data.glutenFree,
+    diets: data.diets
+};
+
+   return recipe;
+
      
 };
 
--- PI-Food-main/api/src/routes/index.js
+++ PI-Food-main/api/src/routes/index.js
@@ -9,25 +9,30 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-router.get('/recipes',  (req, res)=>{
+router.get('/recipes/:idRecipe', async (req, res)=>{
     const {idRecipe} = req.params;
 
     try{
-        res.status(200).json(getRecipeById());
+        res.status(200).json({recipe: await getRecipeById(idRecipe)});
     }catch(error){
         res.status(404).json({err: error.message});
     }
 });
+
 
 router.get('/recipes/',  (req, res)=>{
     const{name} = req.query;
     res.status(200).send(`nombre de receta ${name}`)
 });
 
+
+
 router.post('/recipes', (req, res)=>{
     const recipe = req.body;
     res.status(200).json({nuevaReceta: recipe});
 })
+
+
 
 router.get('/diets',(req, res)=>{
     const {name, age} = req.body;
