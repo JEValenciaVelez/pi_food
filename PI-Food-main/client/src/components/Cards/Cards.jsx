b76a106dcf6bc89408aa9da3e339f3b97c57cc40
Juan Esteban <juancho50301@gmail.com>
2023-05-02 16:35:23 -0500

    'frontend'

--- PI-Food-main/client/src/components/Cards/Cards.jsx
+++ PI-Food-main/client/src/components/Cards/Cards.jsx
@@ -1,14 +1,28 @@
 
+import data from "../../utils/data";
 import Card from "./Card";
 import './Cards.css'
+import Pagination from "./Pagination/Pagination";
 
 const Cards = () => {
+
+    const itemsPerPage = 10;
+    const currentPage = 1;
+    const allItems = data;
+    console.log(data)
+
+    const lastIndex = currentPage * itemsPerPage;
+    const firstIndex = lastIndex - itemsPerPage;
+    const itemsToDisplay = allItems.slice(firstIndex, lastIndex);
+
+
 
     return(
         <div>
             <div className="cards-container">
                 <h1>Recetas Disponibles</h1>
-                <Card/>
+                <Card items={itemsToDisplay}/>
+                <Pagination totalItems={allItems.length} itemsPerPage={itemsPerPage} />
             </div>
         </div>
     )
--- PI-Food-main/client/src/components/Cards/Pagination/Pagination.jsx
+++ PI-Food-main/client/src/components/Cards/Pagination/Pagination.jsx
@@ -1,6 +1,22 @@
 
 
 
-const Pagination = () => {};
+const Pagination = ({ totalItems, itemsPerPage }) => {
+    const totalPages = Math.ceil(totalItems / itemsPerPage);
+    const pages = [];
+  
+    for (let i = 1; i <= totalPages; i++) {
+      pages.push(i);
+    }
+  
+    return (
+      <div className="pagination">
+        {pages.map((page) => (
+          <button key={page}>{page}</button>
+        ))}
+      </div>
+    );
+  };
+  
 
 export default Pagination;
